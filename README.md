e85a522bf72a41b25cee1b9dcf1065ec2f004010
David Csizmadia <firemax12b@gmail.com>
2018-04-15 18:37:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+  <img src="https://github.com/FireMax12/FireSong/blob/master/static/long.png" width="500" />
+</p>
+
+<p align="center">
+  <img src="https://badges.frapsoft.com/os/v2/open-source.png?v=103" />
+  <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg" />
+  <img src="https://img.shields.io/github/package-json/v/FireMax12/FireSong.svg" />
+  <img src="https://img.shields.io/github/languages/code-size/FireMax12/FireSong.svg" />
+  <img src="https://img.shields.io/github/license/FireMax12/FireSong.svg" />
+</p>
+
+## Requirements
+* Python 3.5.x or newer
+* node.js 9.x or newer
+
+## Installation
+```bash
+npm --add-python-to-path='true' --debug install --global windows-build-tools
+npm install
+```
+
+## Usage
+```bash
+npm start
+```
+or simply run `start.vbs`.
+
+## Keybinds
+* **mouse4** - Go back
+* **mouse5** - Go forward
+* **Ctrl + Space** - Start/Stop music _(It also works, when the main window is minimized!)_
---
+++ src/main.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const {app, BrowserWindow, globalShortcut} = require('electron');
+const ioHook = require('iohook');
+const pack = require('../package.json');
+
+app.on('ready', () => {
+  let win = new BrowserWindow({width: 1600, height: 900, icon: __dirname + '/../static/icon.png', title: pack.name});
+  win.setMenu(null);
+
+  win.on('closed', () => {
+    win = null;
+  });
+
+  win.on('page-title-updated', (event, title) => {
+    event.preventDefault();
+    win.setTitle(`${pack.name} v${pack.version} | ${title}`);
+  });
+
+  win.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    win.loadURL(url);
+  });
+
+  win.loadURL('https://www.youtube.com');
+
+  const sendInput = (key) => {
+    win.webContents.sendInputEvent({ type: 'keyDown', keyCode: key });
+    win.webContents.sendInputEvent({ type: 'keyUp', keyCode: key });
+  };
+
+  // Go back, Go forward bindings
+  ioHook.on('mouseup', (event) => {
+    if (!win)
+      return;
+    if (event.button == 4 && win.isFocused())
+      return win.webContents.goBack();
+    if (event.button == 5 && win.isFocused())
+      return win.webContents.goForward();
+  });
+
+  // Start / Stop video with bindings
+  const binding = ioHook.registerShortcut([29, 57], (keys) => { // Bind Ctrl + Space
+    sendInput('\u004B'); // Send 'K' key for YouTube
+  });
+   
+  ioHook.start();
+});
+
+app.on('before-quit', () => {
+  ioHook.unload();
+  ioHook.stop();
+});
